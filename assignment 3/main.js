a3fc1d2af1dd7eb48084bf4ada29f49f50eba125
[syedmazumai1] <[syedumair374@gmail.com]>
2024-03-19 13:56:05 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,5 @@
+// storing the famous quote and its author
+var quote = "Nothing is impossible because impossible itself I M POSSIBLE";
+var author = "mazia umair";
+//printing the message
+console.log("\"".concat(quote, "\"\"one said\"").concat(author));
---
+++ assignment 3/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its author
+
+let quote:string ="Nothing is impossible because impossible itself I M POSSIBLE";
+
+let author:string ="mazia umair";
+
+//printing the message
+
+console.log(`"${quote}""one said"${author}`);
